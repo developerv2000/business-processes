5afe5a31760345dc9db5170d4e9bc388832e35bf
developerv2000 <developerv2000@gmail.com>
2023-12-26 14:13:01 +0500

    Fullscreen target changed

--- resources/views/layouts/breadcrumbs.blade.php
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
 @if ($fullScreen)
-    <span class="material-symbols-outlined prehead__fullscreen" title="{{ __('Full screen mode') }}" data-click-action="request-fullscreen" data-element-target=".main">fullscreen</span>
+    <span class="material-symbols-outlined prehead__fullscreen" title="{{ __('Full screen mode') }}" data-click-action="request-fullscreen" data-element-target=".inner-wrapper">fullscreen</span>
 @endif
 
 <ul class="breadcrumbs">
