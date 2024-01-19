c91396c004ef6343df8f9a7ec10bdb5dda94a37b
developerv2000 <developerv2000@gmail.com>
2024-01-19 15:51:20 +0500

    GLB testing removed

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/glb', [ManufacturerController::class, 'glb']); // TEST GLB
-
 Route::middleware('guest')->controller(AuthenticatedSessionController::class)->group(function () {
     Route::get('/login', 'create')->name('login');
     Route::post('/login', 'store');
